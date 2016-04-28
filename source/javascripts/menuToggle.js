e7c10f80073872bc6fe1c174208c3b2a7919e5a7
Keishi Kuwabara <kuwabara@sackle.co.jp>
2016-04-28 10:26:18 +0900

    画面のサイズを変更するとナビゲーションが消えるバグを修正。

--- source/javascripts/menuToggle.js
+++ source/javascripts/menuToggle.js
@@ -4,14 +4,15 @@ $220(function() {
     return false;
   });
   
-  $220(window).resize(function() {
-   var win = $(window).width();
-   var p = 550;
-   if(win > p) {
-    $220(".main-navigation").show();
-   }
-   else {
-    $220(".main-navigation").hide();
-   }
- });
+//  $220(window).resize(function() {
+//   var win = $(window).width();
+//   var p = 550;
+//   if(win > p) {
+//    $220(".main-navigation").show();
+//   }
+//   else {
+//    $220(".main-navigation").hide();
+//   }
+// });
+
 });
