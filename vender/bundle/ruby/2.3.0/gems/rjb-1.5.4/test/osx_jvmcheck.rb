8dd33f193e02ee06f75b928b965a4ffacc9c0b9c
Keishi Kuwabara <kuwabara@sackle.co.jp>
2016-06-15 19:54:39 +0900

    rubyがpushされていたので消した。余計なディレクトリを消した。

--- vender/bundle/ruby/2.3.0/gems/rjb-1.5.4/test/osx_jvmcheck.rb
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-begin
-  require 'rjb'
-rescue LoadError 
-  require 'rubygems' 
-  require 'rjb'
-end
-S = Rjb::import('java.lang.System')
-puts "#{S.property('java.vendor')} #{S.property('java.version')}"
